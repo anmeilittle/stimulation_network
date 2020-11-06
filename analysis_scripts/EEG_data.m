b41f6c1495b790be8ab3c8742e9c357f7843e2eb
David Huberdeau <david.huberdeau@gmail.com>
2020-11-06 13:41:09 -0500

    added full data file path

--- analysis_scripts/EEG_data.m
+++ analysis_scripts/EEG_data.m
@@ -4,7 +4,7 @@
 % set up workflow
 
 % % load data
-d = h5read('DC_stim_1_512hz.h5', '/data');
+d = h5read('/gpfs/milgram/project/turk-browne/projects/stimulation_behavior/intermediate_data/DC_stim_1_512hz.h5', '/data');
 d = squeeze(d);
 d2 = d(1:203, :);
 
@@ -37,7 +37,7 @@ for i = 1:370
     [~, k_time(i)] = min(abs(timedata - use_times(i)));
 end
 
-% 
+%
 % % let's set the range to be 0.051 s after stimulation for .85s
 % % ex. for 127th stimulation point
 % target_inds = k_time(127) + 26 + (1:435); % this is our range (length 435)
@@ -51,13 +51,3 @@ target_inds = zeros(370, 435);
 for i = 1:370 %for each stimulation
     target_inds(i, :) = k_time(i) + 26 + (1:435);
 end
-
-
-
-
-
-
-
-
-
-
